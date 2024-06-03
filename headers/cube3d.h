57318e6c2eda7e45dbf7c86ec3d1a5b2b22de881
Nicolas Rea <nrea@made-f0Cr3s1.clusters.42paris.fr>
2024-06-03 11:28:21 +0200

    modification message d'erreur invalid char

--- headers/cube3d.h
+++ headers/cube3d.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cube3d.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: qgiraux <qgiraux@student.42.fr>            +#+  +:+       +#+        */
+/*   By: nrea <nrea@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/27 12:56:53 by nrea              #+#    #+#             */
-/*   Updated: 2024/05/31 17:07:46 by qgiraux          ###   ########.fr       */
+/*   Updated: 2024/06/03 11:28:02 by nrea             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,7 +58,7 @@ Scene:;\
 An internal error has occured;\
 Invalid color;\
 parsing texture path issue;\
-Invalid character found at line start;\
+Invalid character;\
 Invalid map character or empty line in map definition;\
 The position can only be set once;\
 The starting position and direction must be set;\
